5734594f727e95f9276c79b7f95f7e0f4a65417e
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-29 10:44:20 -0300

    feat: Model para recebimento dos dados dos veiculos, empresa e movimentação

---
+++ Projeto-API-BackEnd-Estacionamento/Estacionamento.Core/Models/MovimentacaoEstacionamento.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_API_BackEnd_Estacionamento.Estacionamento.Core.Models;
+
+public class MovimentacaoEstacionamento
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O ID do veículo é obrigatório.")]
+    public int VeiculoId { get; set; }
+    [Required(ErrorMessage = "O tipo da vaga é obrigatório.")]
+    public string TipoVaga { get; set; } = string.Empty;
+    [Required(ErrorMessage = "A data de entrada é obrigatória.")]
+    public DateTime DataEntrada { get; set; }   
+    public DateTime? DataSaida { get; set; }
+    [Required(ErrorMessage = "O ID do estabelecimento é obrigatório.")]
+    public int EstabelecimentoId { get; set; }  
+}
+ 
---
+++ Projeto-API-BackEnd-Estacionamento/Estacionamento.Core/Models/Veiculos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_API_BackEnd_Estacionamento.Estacionamento.Core.Models;
+
+public class Veiculos
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Obrigatório informar a marca do veículo.")]
+    public string Marca { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Obrigatório informar o modelo do veículo.")]
+    public string Modelo { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Obrigatório informar a cor do veículo.")]
+    public string Cor { get; set; } = string.Empty;
+    [Required(ErrorMessage = "É obrigatório informar a Placa.")]
+    public string Placa { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Obrigatório informar o tipo do veículo.")]
+    public string Tipo { get; set; } = string.Empty;
+}
