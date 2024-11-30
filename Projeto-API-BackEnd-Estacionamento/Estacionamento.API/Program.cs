412293d8198f34cd5ced1bb0dce64d5ae71b40aa
Foqsz <contatovictorvinicius05@gmail.com>
2024-11-30 09:35:42 -0300

    feat: feito injeção de dependencia

--- Projeto-API-BackEnd-Estacionamento/Estacionamento.API/Program.cs
+++ Projeto-API-BackEnd-Estacionamento/Estacionamento.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Projeto_API_BackEnd_Estacionamento.Estacionamento.Application.Interfaces;
+using Projeto_API_BackEnd_Estacionamento.Estacionamento.Application.Services;
 using Projeto_API_BackEnd_Estacionamento.Estacionamento.Infrastructure.Data;
+using Projeto_API_BackEnd_Estacionamento.Estacionamento.Infrastructure.Repository;
+using Projeto_API_BackEnd_Estacionamento.Estacionamento.Infrastructure.Repository.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,15 @@ builder.Services.AddOpenApi();
 var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<EstacionamentoDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddScoped<IEmpresasRepository, EmpresasRepository>();
+builder.Services.AddScoped<IMovimentacaoRepository, MovimentacaoRepository>();
+builder.Services.AddScoped<IVeiculosRepository, VeiculosRepository>();
+
+builder.Services.AddScoped<IEmpresasService, EmpresasService>();
+builder.Services.AddScoped<IVeiculosService, VeiculosService>();
+builder.Services.AddScoped<IMovimentacaoService, MovimentacaoService>();
+
 
 var app = builder.Build();
 
