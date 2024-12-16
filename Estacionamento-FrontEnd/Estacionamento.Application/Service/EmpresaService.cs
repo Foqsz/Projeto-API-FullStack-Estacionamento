d7e29d5e0b250aaf263f5604cf78d06a5908dfd2
Foqsz <contatovictorvinicius05@gmail.com>
2024-12-16 09:45:37 -0300

    feat: service and interface front end

---
+++ Estacionamento-FrontEnd/Estacionamento.Application/Service/EmpresaService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using Estacionamento_FrontEnd.Estacionamento.Core.Models;
+
+namespace Estacionamento_FrontEnd.Estacionamento.Application.Service;
+
+public class EmpresaService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private const string apiEndPoint = "/api/Empresas";
+    private readonly JsonSerializerOptions _serializerOptions;
+    private EmpresaViewModel _empresaViewModel;
+
+    public EmpresaService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+        _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
+
+
+}
+
---
+++ Estacionamento-FrontEnd/Estacionamento.Application/Service/Interface/IEmpresaService.cs
@@ -0,0 +1,13 @@
+﻿using Estacionamento_FrontEnd.Estacionamento.Core.Models;
+
+namespace Estacionamento_FrontEnd.Estacionamento.Application.Service.Interface
+{
+    public interface IEmpresaService
+    {
+        Task<IEnumerable<EmpresaViewModel>> GetEmpresaAll();
+        Task<EmpresaViewModel> GetEmpresaById(int id);
+        Task<EmpresaViewModel> PostEmpresa(EmpresaViewModel empresa);
+        Task<EmpresaViewModel> PutEmpresa(int id, EmpresaViewModel empresa);
+        Task<bool> DeleteEmpresa(int id);
+    }
+}
