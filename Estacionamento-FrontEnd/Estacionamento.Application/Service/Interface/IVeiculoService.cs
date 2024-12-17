f747d4ab519ec806bf64c7e0062f7ab62752b342
Foqsz <contatovictorvinicius05@gmail.com>
2024-12-17 11:54:36 -0300

    feat: interface para os metodos do front end veiculos

---
+++ Estacionamento-FrontEnd/Estacionamento.Application/Service/Interface/IVeiculoService.cs
@@ -0,0 +1,13 @@
+﻿using Estacionamento_FrontEnd.Estacionamento.Core.Models;
+
+namespace Estacionamento_FrontEnd.Estacionamento.Application.Service.Interface
+{
+    public interface IVeiculoService
+    {
+        Task<IEnumerable<VeiculosViewModel>> GetVeiculosAll();
+        Task<VeiculosViewModel> GetVeiculoById(int id);
+        Task<VeiculosViewModel> PostVeiculo(VeiculosViewModel veiculo);
+        Task<VeiculosViewModel> PutVeculo(int id, VeiculosViewModel veiculo);
+        Task<bool> DeleteVeiculo(int id);
+    }
+}
