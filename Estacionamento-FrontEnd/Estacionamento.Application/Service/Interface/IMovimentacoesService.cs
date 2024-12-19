4986a109d263367c76e82428032fc650438ec726
Foqsz <contatovictorvinicius05@gmail.com>
2024-12-19 09:47:51 -0300

    feat: service and metodo de listagem de todos estacionados

---
+++ Estacionamento-FrontEnd/Estacionamento.Application/Service/Interface/IMovimentacoesService.cs
@@ -0,0 +1,10 @@
+﻿using Estacionamento_FrontEnd.Estacionamento.Core.Models;
+
+namespace Estacionamento_FrontEnd.Estacionamento.Application.Service.Interface;
+
+public interface IMovimentacoesService
+{
+    Task<IEnumerable<MovimentacaoViewModel>> GetEstacionadosAll();
+    Task<MovimentacaoViewModel> RegistrarEntrada();
+    Task<MovimentacaoViewModel> RegistrarSaida();
+}
---
+++ Estacionamento-FrontEnd/Estacionamento.Application/Service/MovimentacoesService.cs
@@ -0,0 +1,54 @@
+﻿using Estacionamento_FrontEnd.Estacionamento.Application.Service.Interface;
+using Estacionamento_FrontEnd.Estacionamento.Core.Models;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace Estacionamento_FrontEnd.Estacionamento.Application.Service
+{
+    public class MovimentacoesService : IMovimentacoesService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private const string apiEndPoint = "/api/Movimentacao";
+        private readonly JsonSerializerOptions _serializerOptions;
+
+        public MovimentacoesService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        public async Task<IEnumerable<MovimentacaoViewModel>> GetEstacionadosAll()
+        {
+            var client = myHttpClient();
+
+            var response = await client.GetAsync($"{apiEndPoint}/Estacionados");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<IEnumerable<MovimentacaoViewModel>>(content);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Task<MovimentacaoViewModel> RegistrarEntrada()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MovimentacaoViewModel> RegistrarSaida()
+        {
+            throw new NotImplementedException();
+        }
+
+        private HttpClient myHttpClient()
+        {
+            var client = _httpClientFactory.CreateClient("EstacionamentoApi");
+            return client;
+        }
+    }
+}
