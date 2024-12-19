ed24f10699cfaa5f29e862d567596b41ed2e392b
Foqsz <contatovictorvinicius05@gmail.com>
2024-12-19 09:47:33 -0300

    feat: controller

---
+++ Estacionamento-FrontEnd/Estacionamento.API/Controllers/MovimentacoesController.cs
@@ -0,0 +1,24 @@
+﻿using Estacionamento_FrontEnd.Estacionamento.Application.Service.Interface;
+using Estacionamento_FrontEnd.Estacionamento.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estacionamento_FrontEnd.Estacionamento.API.Controllers
+{
+    public class MovimentacoesController : Controller
+    {
+        private readonly IMovimentacoesService _movimentacoesService;
+
+        public MovimentacoesController(IMovimentacoesService movimentacoesService)
+        {
+            _movimentacoesService = movimentacoesService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MovimentacaoViewModel>>> Index()
+        {
+            var estacionadosAll = await _movimentacoesService.GetEstacionadosAll();
+
+            return estacionadosAll is null ? View("Error") : View(estacionadosAll);
+        }
+    }
+}
